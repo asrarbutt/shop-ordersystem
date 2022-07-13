48a79a61e752168220c96f4f348fd59c3f8961ea
asrarbutt <asrarbutt1986@gmail.com>
2022-07-13 21:04:29 +0200

    add all methods for orderRepo and productRepo

--- src/main/java/shop/ShopService.java
+++ src/main/java/shop/ShopService.java
@@ -1,9 +1,11 @@
 package shop;
 
+import shop.order.Order;
 import shop.order.OrderRepo;
 import shop.product.Product;
 import shop.product.ProductRepo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ShopService {
@@ -21,6 +23,27 @@ public class ShopService {
 
     public List<Product> listProducts(){
         return productRepo.productList();
+    }
+
+    public void addOrder(int orderId, List<Integer> productIds){
+        List<Product> products=new ArrayList<>();
+
+        for (int productid : productIds){
+            Product product=productRepo.getProduct(productid);
+            products.add(product);
+        }
+
+        Order order=new Order(orderId, products);
+        orderRepo.addOrder(order);
+    }
+
+    public Order getOrder(int orderId){
+        return orderRepo.getOrder(orderId);
+
+    }
+
+    public List<Order> listOrder(){
+        return orderRepo.listOrders();
     }
 
 }
